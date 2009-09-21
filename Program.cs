007f282d8ecc21a77e23cbb652ab00ea49489227
shishkin <sergei.shishkin@gmail.com>
2009-09-21 23:19:26 +0200

    first commit

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var megabytes = 40.5;
+
+            bool ok = CheckFreeSpace(megabytes);
+            Console.WriteLine(ok);
+        }
+
+        private static bool CheckFreeSpace(double megabytes)
+        {
+            var currentDirectory = Directory.GetCurrentDirectory();
+            var driveName = Path.GetPathRoot(currentDirectory);
+            var drive = new DriveInfo(driveName);
+            checked
+            {
+                var requiredBytes = (long)megabytes * 1048576L;
+                return drive.AvailableFreeSpace > requiredBytes;
+            }
+        }
+    }
+}
